84aebbfaaddf3e698cb92efd31a34fd0ca0728bd
tommyotsai <tommy@trykeet.com>
2025-10-01 11:12:05 -0400

    remove

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -3,11 +3,10 @@ import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
 import { asciiWorkflow, createAsciiWorkflow } from './asciimatics/asciiWorkflow';
-import { createManimGameWorkflow } from './manim/manimWorkflow';
 
 
 export const mastra = new Mastra({
-  workflows: { createAsciiWorkflow, asciiWorkflow, createManimGameWorkflow },
+  workflows: { createAsciiWorkflow, asciiWorkflow },
   agents: { },
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
